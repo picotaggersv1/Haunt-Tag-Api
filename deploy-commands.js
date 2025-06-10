c0c411c7962ed825cb78c121969b1aa709d598d0
picotaggersv1 <therealnicole6@gmail.com>
2025-06-10 05:06:01 -0800

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,7 +4,119 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const commands = [];
-// Grab all the command folders from the commands directory you created earlier
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('check')
+		.setDescription('Checks to see if the bot is online by replying'),
+	async execute(interaction) {
+		await interaction.reply('Hello');
+	},
+};
+const { SlashCommandBuilder } = require('discord.js');
+const PlayFab = require('playfab-sdk/Scripts/PlayFab/PlayFab');
+const PlayFabAdmin = require('playfab-sdk/Scripts/PlayFab/PlayFabAdmin');
+
+PlayFab.settings.titleId = ''; // Replace with your PlayFab Title ID
+PlayFab.settings.developerSecretKey = ''; // Replace with your PlayFab Secret Key
+
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('playfabban')
+    .setDescription('Bans a player on playfab')
+    .addStringOption(option =>
+      option.setName('playerid')
+        .setDescription('The ID of the player to ban')
+        .setRequired(true))
+    .addIntegerOption(option =>
+      option.setName('banduration')
+        .setDescription('The ban duration in hours')
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName('banreason')
+        .setDescription('The reason for the ban')
+        .setRequired(true)),
+  async execute(interaction) {
+    if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
+      return interaction.reply('no thanks');
+    }
+
+    const playerId = interaction.options.getString('playerid');
+    const banDurationInHours = interaction.options.getInteger('banduration');
+    const banReason = interaction.options.getString('banreason');
+
+    try {
+
+      const banResult = await PlayFabAdmin.BanUsers({
+        Bans: [
+          {
+            PlayFabId: playerId,
+            Reason: banReason,
+            DurationInHours: banDurationInHours,
+          },
+        ],
+      });
+
+
+      await interaction.reply(`Okay, I banned ${playerId}. Result: ${JSON.stringify(banResult)}`);
+    } catch (error) {
+      console.error('Error banning user:', error);
+      await interaction.reply(`Uh oh, an error occurred: ${error.errorMessage}`);
+    }
+  },
+};
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('status')
+    .setDescription('Changes my status')
+    .addStringOption(option =>
+      option.setName('change')
+        .setDescription('Choose DND, Online, or Idle')
+        .setRequired(true)),
+  async execute(interaction) {
+
+    if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
+      return interaction.reply('No thanks');
+    }
+
+    const allowedStatuses = ['dnd', 'online', 'idle'];
+    const selectedStatus = interaction.options.getString('change').toLowerCase();
+
+    if (!allowedStatuses.includes(selectedStatus)) {
+      return interaction.reply('Please choose dnd, online, or idle.');
+    }
+
+    try {
+      if (selectedStatus === 'dnd') {
+        await interaction.client.user.setPresence({ status: 'dnd' });
+      } else if (selectedStatus === 'online') {
+        await interaction.client.user.setPresence({ status: 'online' });
+      } else if (selectedStatus === 'idle') {
+        await interaction.client.user.setPresence({ status: 'idle' });
+      }
+      interaction.reply(`I am now ${selectedStatus}.`);
+    } catch (error) {
+      console.error('Something went wrong', error);
+      interaction.reply('Something went wrong.');
+    }
+  }
+};
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('userinfo')
+		.setDescription('I know everything about you.'),
+	async execute(interaction) {
+		await interaction.reply(`ok ${interaction.user.username}, You joined on ${interaction.member.joinedAt}.`);
+	},
+};
+
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
