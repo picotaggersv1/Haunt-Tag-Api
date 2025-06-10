a1479de61b7abac54a5c7578d3c524d35c3f38f2
picotaggersv1 <therealnicole6@gmail.com>
2025-06-10 04:35:37 -0800

    Update userinfo.js

--- Commands/Utlity /userinfo.js
+++ Commands/Utlity /userinfo.js
@@ -1,5 +1,10 @@
-{
-	"token": "BOT TOKEN HERE",
-	"clientId": "BOTS USER ID HERE",
-	"guildId": "YOUR DISCORD SERVER ID HERE"
-}
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('userinfo')
+		.setDescription('I know everything about you.'),
+	async execute(interaction) {
+		await interaction.reply(`ok ${interaction.user.username}, You joined on ${interaction.member.joinedAt}.`);
+	},
+};
